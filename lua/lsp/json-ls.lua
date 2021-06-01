2bb2bded221c8d32b1e73b5c6cb753d207361590
lucaschreiner <lucaschreiner06@web.de>
2021-06-01 22:15:39 +0200

    updates

--- lua/lsp/json-ls.lua
+++ lua/lsp/json-ls.lua
@@ -1,14 +1,2 @@
 -- npm install -g vscode-json-languageserver
-require'lspconfig'.jsonls.setup {
-    cmd = {
-        "--stdio"
-    },
-    on_attach = require'lsp'.common_on_attach,
-    commands = {
-        Format = {
-            function()
-                vim.lsp.buf.range_formatting({}, {0, 0}, {vim.fn.line("$"), 0})
-            end
-        }
-    }
-}
+require'lspconfig'.jsonls.setup {}
--- lua/lsp/python-ls.lua
+++ lua/lsp/python-ls.lua
@@ -1,12 +1,2 @@
 -- npm i -g pyright
-require'lspconfig'.pyright.setup {
-    on_attach = require'lsp'.common_on_attach,
-    handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-            virtual_text = true,
-            signs = true,
-            underline = true,
-            update_in_insert = true
-        })
-    }
-}
+require'lspconfig'.pyright.setup {}
